c4c4ad5ed25fa2846764d6304486dcc58ef3b177
Shark Boi <immortalwarrior101@gmail.com>
2020-06-09 16:49:22 +0400

    Moved files being marked as deleted

--- src/methods/argError.ts
+++
@@ -1,7 +0,0 @@
-function argError(action, err) {
-  return new Error(`Action Argument Error [${action}]: ${err}`);
-}
-
-module.exports = function(action) {
-  return argError.bind(argError, action);
-}
