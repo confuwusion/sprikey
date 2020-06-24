367a96b3dbd2c1f8c5cf9c446814143c4696d667
Shark Boi <immortalwarrior101@gmail.com>
2020-06-24 14:43:33 +0400

    Moved file to schema structs dir

--- src/structs/db/SchemaTypes.ts
+++
@@ -1,4 +0,0 @@
-import { DatabaseCache } from "./DatabaseHandler";
-
-export type StaticSchemaType<T> = DatabaseCache<keyof T, T[keyof T]>;
-export type MappedSchemaType<K, V> = DatabaseCache<K, V>;
