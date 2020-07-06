2d3cc015ff7b40cbe5731079a427ea88aab8b39f
Shark Boi <immortalwarrior101@gmail.com>
2020-07-06 23:01:31 +0400

    Added relations in criteria property
    - Set criteria.targets and criteria.includes as relations to entities of their own
    - Added a feature to fetch and set matching criteria instances as relations as an optimization

--- src/structs/db/entities/Watcher.ts
+++ src/structs/db/entities/Watcher.ts
@@ -1,61 +1,144 @@
-import {Column, Entity, PrimaryColumn} from "typeorm";
-import {BaseEntry} from "../BaseEntry";
+import {Column, Entity, PrimaryColumn, OneToOne, BaseEntity, getRepository, JoinColumn} from "typeorm";
 import {ActionRegistry} from "../../typedefs/Action";
-
-
-interface CriteriaInstances<T> {
-  channels?: T,
-  members?: T,
-  roles?: T
-}
 
 type RegExpFlags = `i` | ``;
 
-class WatchData extends BaseEntry<WatchData> {
-
-  @Column()
-  readonly pattern: [ string, RegExpFlags ];
-
-  @Column()
-  readonly criteria?: {
-    readonly targets?: CriteriaInstances<string[]>,
-    readonly include?: CriteriaInstances<boolean>
-  }
-
-  @Column()
-  readonly options: {
-    readonly actions?: ActionRegistry[],
-    readonly clean?: boolean,
-    readonly tags?: { [index: string]: string }
-  };
-
-  constructor({
-    pattern,
-    criteria: {
-      targets: { channels = [], members = [], roles = [] },
-      include: { channels: cB = true, members: mB = true, roles: rB = true }
-    } = { targets: {}, include: {} },
-    options: {
-      actions,
-      clean = false,
-      tags = {}
-    } = {}
-  }: WatchData) {
-    super({
-      pattern,
-      criteria: {
-        targets: { channels, members, roles },
-        include: { channels: cB, members: mB, roles: rB }
-      },
-      options: { actions, clean, tags }
-    });
-  }
-}
-
-@Entity()
-export class WatchEntry extends WatchData {
+class CodedColumn extends BaseEntity {
 
   @PrimaryColumn()
   readonly code: string = process.hrtime.bigint().toString(36);
 
+}
+
+module Criteria {
+
+  interface InstanceType<T> {
+    channels: T,
+    members: T,
+    roles: T
+  }
+
+  export interface CriteriaType {
+    targets: InstanceType<string[]>,
+    includes: InstanceType<boolean>
+  }
+
+  export type CriteriaPromiseType = {
+    [K in keyof CriteriaType]: Promise<CriteriaType[K]>
+  }
+
+  class Instances<T> extends CodedColumn {
+
+    @Column()
+    readonly channels: T;
+
+    @Column()
+    readonly members: T;
+
+    @Column()
+    readonly roles: T;
+
+    constructor({ channels, members, roles }: InstanceType<T>) {
+      super();
+
+      this.channels = channels;
+      this.members = members;
+      this.roles = roles;
+    }
+
+  }
+
+  @Entity()
+  class Targets extends Instances<string[]> {
+
+    static async findOrMakeOne(entry: Targets) {
+      return (await Targets.findOne(entry))
+        || (new Targets(entry)).save();
+    }
+
+  }
+
+  @Entity()
+  class Includes extends Instances<boolean> {
+
+    static async findOrMakeOne(entry: Includes) {
+      return (await Includes.findOne(entry))
+        || (new Includes(entry)).save();
+    }
+
+
+  }
+
+  @Entity()
+  export class Instance extends CodedColumn {
+
+    @OneToOne(() => Targets)
+    @JoinColumn()
+    targets: Promise<CriteriaType["targets"]>;
+
+    @OneToOne(() => Includes)
+    @JoinColumn()
+    includes: Promise<CriteriaType["includes"]>;
+
+    constructor(targets: Targets, includes: Includes) {
+      super();
+
+      this.targets = Targets.findOrMakeOne(targets);
+      this.includes = Includes.findOrMakeOne(includes);
+    }
+
+  }
+}
+
+export class WatchEntry extends CodedColumn {
+
+  @Column()
+  readonly pattern: Promise<[ string, RegExpFlags ]>;
+
+  @OneToOne(() => Criteria.Instance)
+  @JoinColumn()
+  readonly criteria: Criteria.CriteriaPromiseType;
+
+  @Column()
+  readonly actions: ActionRegistry[];
+
+  @Column()
+  readonly options: {
+    readonly clean: boolean,
+    readonly tags: { [index: string]: string }
+  };
+
+  constructor(
+    pattern: WatchEntry["pattern"],
+    {
+      targets: {
+        channels = [],
+        members = [],
+        roles = []
+      } = { channels: [], members: [], roles: [] },
+      includes: {
+        channels: cB = false,
+        members: mB = false,
+        roles: rB = false
+      } = { channels: false, members: false, roles: false }
+    }: Criteria.CriteriaType = {
+      targets: { channels: [], members: [], roles: [] },
+      includes: { channels: false, members: false, roles: false }
+    },
+    actions: WatchEntry["actions"],
+    {
+      clean = false,
+      tags = {}
+    }: WatchEntry["options"] = { clean: false, tags: {} }
+  ) {
+    super();
+
+    this.pattern = pattern;
+    this.actions = actions;
+    this.criteria = {
+      targets: Promise.resolve({ channels, members, roles }),
+      includes: Promise.resolve({ channels: cB, members: mB, roles: rB })
+    };
+    this.options = { clean, tags };
+  }
 }
