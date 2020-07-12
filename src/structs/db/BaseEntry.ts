637c4e2fc36fe54646c8f8175158eee33c7dbc4a
Shark Boi <immortalwarrior101@gmail.com>
2020-07-12 13:43:27 +0400

    BaseEntry now extends BaseEntity as a shorthand to inherit repository properties

--- src/structs/db/BaseEntry.ts
+++ src/structs/db/BaseEntry.ts
@@ -1,5 +1,8 @@
-export class BaseEntry<T> {
+import {BaseEntity} from "typeorm";
+
+export class BaseEntry<T> extends BaseEntity {
   constructor(entries: T) {
+    super();
     Object.assign(this, entries);
   }
 }
