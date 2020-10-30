690c6c4e112a345f056017b80d56df70835a7fe8
Spuggle <immortalwarrior101@gmail.com>
2020-10-30 19:36:11 +0500

    Move cache generation code to a separate file

---
+++ src/db/lib/cache.ts
@@ -0,0 +1,30 @@
+import { PermissionCheck } from "@structs/typedefs/Command";
+import { SizedArray } from "@util/SizedArray";
+import { Role } from "discord.js";
+
+/**
+ *
+ */
+export function generateBotCache(): CacheEntries {
+  return {
+    data: {},
+    logs: {
+      permissionChecks: new SizedArray(100)
+    },
+    roles: {}
+  };
+}
+
+interface CacheEntries {
+  data: CacheDataEntries;
+  logs: CacheLogEntries;
+  roles: { [K: string]: Role | null };
+}
+
+interface CacheDataEntries {
+  [K: string]: unknown;
+}
+
+interface CacheLogEntries {
+  permissionChecks: SizedArray<PermissionCheck>;
+}
