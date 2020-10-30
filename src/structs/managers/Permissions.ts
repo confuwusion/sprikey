2832eaec77d127f0272260cecf87fecfb2880792
Spuggle <immortalwarrior101@gmail.com>
2020-10-30 13:45:03 +0500

    Refactoring and integrating the new db system

--- src/structs/managers/Permissions.ts
+++ src/structs/managers/Permissions.ts
@@ -1,52 +1,75 @@
-const { CacheMap } = require("./CacheMap.js");
-const {
-  PERMISSIONS: {
-    HIERARCHIES: { MASTER, EVERYONE }, 
-    TRENDS: { CURRENT_BELOW, CURRENT_ONLY, CURRENT_ABOVE, TREND_OFFSET }
-  }
-} = require(`../constants.js`);
+import { PERMISSIONS } from "@constants/Permissions";
+import { CommandHierarchy } from "@entities/CommandHierarchies";
+import { SprikeyClient } from "@structs/SprikeyClient";
+import { Collection } from "discord.js";
 
-const trendComparison = [
-  (a, b) => a >= b,
-  (a, b) => a === b,
-  (a, b) => a <= b
+const NO_COMMAND_HIERARCHIES = new Collection<string, number>();
+
+const trendComparers: ((comparingID: number, baseID: number) => boolean)[] = [
+  (comparingID, baseID): boolean => comparingID >= baseID,
+  (comparingID, baseID): boolean => comparingID === baseID,
+  (comparingID, baseID): boolean => comparingID <= baseID
 ];
 
-class PermissionsManager extends CacheMap {
-  constructor(data, cache) {
-    super(data, cache);
-  }
-  
-  getOrMake(id, permissions = {}) {
-    const data = this.get(id);
-    
-    if (!data) this.set(id, { baseHierarchy: EVERYONE, commandHierarchies: {} });
-    
-    return this.get(id);
-  }
-  
-  forCommand(commandName, memberID) {
-    const { baseHierarchy, commandHierarchies } = this.getOrMake(memberID);
-    
-    const commandHierarchy = commandHierarchies[commandName];
-      
-    return baseHierarchy === MASTER
-      ? MASTER
-      : commandHierarchy || baseHierarchy || EVERYONE;
-  }
-  
-  compare(baseID, trend, comparingID) {
-    const comparer = trendComparison[trend - TREND_OFFSET];
-    
-    return comparingID === 1 || comparer(comparingID, baseID);
-  }
-  
-  hasLowerHierarchy(targetMemberID, comparingMemberID, commandName) {
-    const targetHierarchy = this.forCommand(commandName, targetMemberID);
-    const comparingHierarchy = this.forCommand(commandName, comparingMemberID);
-    
-    return this.compare(targetHierarchy, CURRENT_BELOW, comparingHierarchy);
-  }
-}
+export class PermissionsManager {
 
-module.exports = {PermissionsManager};
+  readonly commandHierarchies = new Collection<string, CommandHierarchy["commandHierarchies"]>();
+
+  readonly memberHierarchies = new Collection<string, number>();
+
+  constructor(readonly client: SprikeyClient, permissionEntries: CommandHierarchy[]) {
+    for (const { memberID, commandHierarchies } of permissionEntries) {
+      this.commandHierarchies.set(memberID, commandHierarchies);
+    }
+  }
+
+  async getCommandHierarchies(memberID: string): Promise<CommandHierarchy["commandHierarchies"]> {
+    if (!this.commandHierarchies.has(memberID)) {
+      const retrievedHierarchy = await this.client.db.CommandHierarchies.findOne({ memberID });
+
+      if (retrievedHierarchy) this.commandHierarchies.set(memberID, retrievedHierarchy.commandHierarchies);
+    }
+    const commandHierarchy = this.commandHierarchies.get(memberID);
+
+    return commandHierarchy ?? NO_COMMAND_HIERARCHIES;
+  }
+
+  async forCommand(commandName: string, memberID: string): Promise<number> {
+    const commandHierarchies = await this.getCommandHierarchies(memberID);
+    const commandHierarchy = commandHierarchies.get(commandName);
+
+    const memberHierarchy = this.memberHierarchies.get(memberID);
+
+    return memberHierarchy === PERMISSIONS.HIERARCHIES.MASTER
+      ? PERMISSIONS.HIERARCHIES.MASTER
+      : commandHierarchy || memberHierarchy || PERMISSIONS.HIERARCHIES.EVERYONE;
+  }
+
+
+  async hasLowerHierarchy(
+    targetMemberID: string,
+    comparingMemberID: string,
+    commandName: string
+  ): Promise<boolean> {
+    const [
+      comparingHierarchy,
+      targetHierarchy
+    ] = await Promise.all([
+      this.forCommand(commandName, targetMemberID),
+      this.forCommand(commandName, comparingMemberID)
+    ]);
+
+    return PermissionsManager.compare(
+      targetHierarchy,
+      PERMISSIONS.TRENDS.CURRENT_BELOW,
+      comparingHierarchy
+    );
+  }
+
+  static compare(baseID: number, trend: number, comparingID: number): boolean {
+    const compareTrends = trendComparers[trend - PERMISSIONS.TRENDS.TREND_OFFSET];
+
+    return comparingID === 1 || compareTrends(comparingID, baseID);
+  }
+
+}
