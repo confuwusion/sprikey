0457d1e8347d59a4ba30db727a27ccbdc5e16345
Spuggle <immortalwarrior101@gmail.com>
2020-10-31 18:38:40 +0500

    [Bot] Rewrote main file

--- src/bot.ts
+++ src/bot.ts
@@ -1,163 +1,118 @@
+import "./configs/setupenv";
 import "reflect-metadata";
-import * as CONSTANTS from "./constants";
-import * as Keyv from "keyv";
-import * as bufferJSON from "json-buffer";
-import * as express from "express/index";
-import * as sqlite3 from "sqlite3/index";
-import { BotPack } from "./structures/BotPack";
-import { Client } from "discord.js";
-import { DataPack } from "./structures/DataPack";
-import { deserialize, serialize } from "v8";
 
+import { GUILD } from "@constants/Guild";
+import { ROLES } from "@constants/Roles";
+import { initiateConnection } from "@db/connection";
+import { SprikeyClient } from "@structs/SprikeyClient";
+import { Guild, Role } from "discord.js";
 
-const {
-  COMMUNIMATE_ID,
-  TEST_GUILD_ID,
-  ROLES: { ADMIN, MOD, D_ADMIN, D_MOD }
-} = CONSTANTS;
+async function main(): Promise<void> {
+  const client = await prepare();
 
-// Database
-const dbFile = `./data/sqlite.db`;
-const dbHandler = sqlite3.verbose();
-const sqlDB = new dbHandler.Database(dbFile);
-const database = new Keyv(`sqlite://./data/sqlite.db`, {
-  serialize(data: any) {
-    const serialized = serialize(data);
-    return bufferJSON.stringify(serialized);
-  },
-  deserialize(data: string) {
-    const dataBuffer: NodeJS.TypedArray = bufferJSON.parse(data);
-    return deserialize(dataBuffer);
-  }
-});
+  console.time(`Bot Login`);
+  client.login(process.env.BOT_TOKEN as string)
+    .catch(err => {
+      console.error(err);
+      console.timeEnd(`Bot Login`);
+    });
 
-database.on(`error`, (err: any) => console.error(`Keyv connection error:`, err));
+}
 
-// ExpressJS
-const app = express();
-app.use(express.static(`public`));
-app.get(`/`, (_request, response) => {
-  response.sendFile(`${__dirname}/views/index.html`);
-});
+async function prepare(): Promise<SprikeyClient> {
+  const connection = await initiateConnection();
+  const client = new SprikeyClient(connection);
 
-const listener: any = app.listen(
-  process.env.PORT,
-  () => console.log(`Your app is listening on port ${listener.address().port}`)
-);
+  client.once(`ready`, () => {
+    ready(client)
+      .catch(async err => {
+        console.error(err);
 
-// Client
-const client = new SpuiiClient();
+        return client.user?.setPresence({
+          status: `dnd`,
+          activity: {
+            name: `out for errors`,
+            type: `WATCHING`
+          }
+        });
+      });
+  });
 
-async function ready() {
-  console.timeEnd("Bot Login");
-  console.time("Initiation");
-  console.log("Bot Connected!");
+  return client;
+}
 
-  // Fetch client user if it is not cached
-  !client.user.partial && client.user.fetch();
+async function ready(client: SprikeyClient): Promise<void> {
+  console.timeEnd(`Bot Login`);
+  console.time(`Initiation`);
+  console.log(`Bot Connected!`);
 
-  // Load all important data from Discord
-  const MAIN_GUILD = client.guilds.cache.get(COMMUNIMATE_ID);
-  const TEST_GUILD = client.guilds.cache.get(TEST_GUILD_ID);
+  await loadAndCacheStaffRoles(client);
 
-  const BLANK_ROLE = { members: new Map() };
+  // cache.bot_last_active_interval = setInterval(() => {
+  //   cache.botLastActive = Date.now();
+  // }, 15000);
+
+  // cache.subcommands.toArray.entries().map(([ subcommandName, {
+  //   inherits,
+  //   ...subcommandData
+  // } ]) => {
+  //   const subbingCommand = dataPack.commands.get(inherits);
+  //   const subCommand = subbingCommand.createSub(subcommandData);
+
+  //   dataPack.commands.set(subcommandName, subCommand);
+  // });
+
+  await enableBot(client);
+
+  // if (cache.restartMessage.channelID) {
+  //   const { channelID, messageID } = cache.restartMessage;
+  //   const restartMessage = await client.channels.cache.get(channelID).messages.fetch(messageID);
+
+  //   if (restartMessage) {
+  //     (async function() {
+  //       await restartMessage.reactions.removeAll();
+  //       await restartMessage.react(`709510035960496149`);
+  //     })();
+
+  //     delete cache.restartMessage;
+  //     cache.save(`restartMessage`);
+  //   }
+  // }
+
+  console.log(`Bot is now listening to events!`);
+  console.timeEnd(`Initiation`);
+}
+
+void main();
+
+async function loadAndCacheStaffRoles(client: SprikeyClient): Promise<void> {
+  const MAIN_GUILD = client.guilds.cache.get(GUILD.MAIN);
+  const TEST_GUILD = client.guilds.cache.get(GUILD.TEST) as Guild;
+
+  const BLANK_ROLE = new Role(client, {}, TEST_GUILD);
 
   const [
     mainAdmin, mainMod,
     testAdmin, testMod
   ] = await Promise.all([
-    MAIN_GUILD ? MAIN_GUILD.roles.fetch(ADMIN) : BLANK_ROLE,
-    MAIN_GUILD ? MAIN_GUILD.roles.fetch(MOD) : BLANK_ROLE,
-    TEST_GUILD ? TEST_GUILD.roles.fetch(D_ADMIN) : BLANK_ROLE,
-    TEST_GUILD ? TEST_GUILD.roles.fetch(D_MOD) : BLANK_ROLE
+    MAIN_GUILD ? MAIN_GUILD.roles.fetch(ROLES.MAIN.STAFF.ADMIN) : BLANK_ROLE,
+    MAIN_GUILD ? MAIN_GUILD.roles.fetch(ROLES.MAIN.STAFF.MOD) : BLANK_ROLE,
+    TEST_GUILD.roles.fetch(ROLES.TEST.STAFF.ADMIN),
+    TEST_GUILD.roles.fetch(ROLES.TEST.STAFF.MOD)
   ]);
 
-  // Initiating and loading cache
-  const cache = require("./cache.js")(client, database);
-
-  cache.roles = { mainAdmin, mainMod, testAdmin, testMod };
-
-  console.time("Cache Load");
-  cache.data_load_status = await cache.loadMissing();
-  console.timeEnd("Cache Load");
-
-  // Initiating Data pack
-  const botPack = new BotPack(cache, client, MAIN_GUILD, TEST_GUILD);
-  client.botPack = botPack;
-
-  const dataPack = new DataPack(botPack);
-  client.dataPack = dataPack;
-
-  // Loading instances from project dir
-  const loader = require("./loader.js")(dataPack);
-  const loaded = {
-    events: loader.load("./events", loader.registerEvent),
-    commands: loader.load("./commands", loader.registerCommand),
-    actions: loader.load("./actions", loader.registerAction),
-    timeEvents: loader.load("./timeEvents", loader.registerTimeEvent)
-  };
-  cache.loaded_instances = loaded;
-
-  cache.loaded_missed_events = dataPack.timeManager.loadMissed();
-
-  cache.bot_last_active_interval = setInterval(() => {
-    cache.botLastActive = Date.now();
-    cache.save("botLastActive");
-  }, 15000);
-
-  // Setting subcommands
-  cache.subcommands.toArray.entries().map(([ subcommandName, {
-    inherits,
-    ...subcommandData
-  } ]) => {
-    const subbingCommand = dataPack.commands.get(inherits);
-    const subCommand = subbingCommand.createSub(subcommandData);
-
-    dataPack.commands.set(subcommandName, subCommand);
-  });
-
-  // Show status as available
-  await client.user.setPresence({
-    status: "online",
-    activity: {
-      name: "with butterflies",
-      type: "PLAYING"
-    }
-  });
-
-  // Accept events
-  botPack.eventState = true;
-
-  if (cache.restartMessage.channelID) {
-    const { channelID, messageID } = cache.restartMessage;
-    const restartMessage = await client.channels.cache.get(channelID).messages.fetch(messageID);
-
-    if (restartMessage) {
-      (async function() {
-        await restartMessage.reactions.removeAll();
-        await restartMessage.react("709510035960496149");
-      })();
-
-      delete cache.restartMessage;
-      cache.save("restartMessage");
-    }
-  }
-
-  console.log("Bot is now listening to events!");
-  console.timeEnd("Initiation");
+  client.cache.roles = { mainAdmin, mainMod, testAdmin, testMod };
 }
 
-client.on("ready", () => {
-  ready()
-  .catch(err => console.error(err) || client.user.setPresence({
-    status: "dnd",
-    activity: {
-      name: "out for errors",
-      type: "WATCHING"
-    }
-  }));
-});
 
-console.time("Bot Login");
-client.login(process.env.TOKEN)
-  .catch(err => (console.error(err), console.timeEnd("Bot Login")));
+async function enableBot(client: SprikeyClient): Promise<void> {
+  await client.user?.setPresence({
+    status: `online`,
+    activity: {
+      name: `with butterflies`,
+      type: `PLAYING`
+    }
+  });
+
+  client.managers.listener.changeAllStatesTo(`enabled`);
+}
