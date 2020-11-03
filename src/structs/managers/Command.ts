7756da58fb6592f901b00bd1a0cf1d87190347a5
Spuggle <immortalwarrior101@gmail.com>
2020-11-03 13:02:12 +0500

    [Manager] Refactoring, dynamically add categories
    - Commands now get added to their respective categories

--- src/structs/managers/Command.ts
+++ src/structs/managers/Command.ts
@@ -1,31 +1,38 @@
+import { CommandsCommand } from "@commands/General/commands";
+import HelpCommand from "@commands/General/help";
 import PingCommand from "@commands/General/ping";
 import ExecCommand from "@commands/Master/exec";
+import { LogCommand } from "@commands/Moderation/log";
 import { SprikeyClient } from "@structs/SprikeyClient";
-import { Categories, Command } from "@structs/typedefs/Command";
+import { Categories } from "@structs/typedefs/Command";
 import { Collection } from "discord.js";
 
 const commandClasses = {
-  PingCommand: new PingCommand(),
-  ExecCommand: new ExecCommand()
+  CommandsCommand: new CommandsCommand(),
+  ExecCommand: new ExecCommand(),
+  HelpCommand: new HelpCommand(),
+  LogCommand: new LogCommand(),
+  PingCommand: new PingCommand()
 } as const;
 
 export class CommandManager {
 
-  readonly categories: Collection<Categories, this["commands"]> = new Collection(
-    Object.keys(Categories).map(category => [
-      Categories[category as CategoryKeys],
-      new Collection()
-    ])
-  );
+  readonly categories: Map<CategoryKeys, this["commands"]> = new Map<CategoryKeys, this["commands"]>();
 
-  readonly commands = new Collection<string, Command<object>>();
+  readonly commands = new Collection<string, AvailableCommands>();
 
   constructor(readonly client: SprikeyClient) {
     for (const command of Object.values(commandClasses)) {
       this.commands.set(command.name, command);
+
+      const selectedCategory = Categories[command.category] as CategoryKeys;
+      if (!this.categories.has(selectedCategory)) this.categories.set(selectedCategory, new Collection());
+
+      this.categories.get(selectedCategory)?.set(command.name, command);
     }
   }
 
 }
 
+export type AvailableCommands = (typeof commandClasses)[keyof typeof commandClasses];
 type CategoryKeys = keyof typeof Categories;
