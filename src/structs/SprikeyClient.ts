eeaa541d99c7ee4a508bca20688f8aab56e21334
spuggle <immortalwarrior101@gmail.com>
2020-11-22 15:23:35 +0500

    [SprikeyClient] Integrate Akairo

--- src/structs/SprikeyClient.ts
+++ src/structs/SprikeyClient.ts
@@ -1,16 +1,19 @@
 import { GUILD } from "@constants/Guild";
+import { MASTER_ID } from "@constants/Team";
 import { TableMetadata } from "@db/config";
 import { InitiatedConnection } from "@db/connection";
 import { generateBotCache } from "@db/lib/cache";
 import { OptionsManager } from "@db/lib/managers/Options";
-import { Client, Guild } from "discord.js";
+import { AppState } from "app";
+import { AkairoClient, CommandHandler, InhibitorHandler, ListenerHandler } from "discord-akairo";
+import { Guild } from "discord.js";
 import { EventEmitter } from "events";
+import * as path from "path";
 
-import { CommandManager } from "./managers/Command";
-import { ListenerManager } from "./managers/Listeners";
+import SprikeyCommandHandler from "./handlers/SprikeyCommandHandler";
 import { PermissionsManager } from "./managers/Permissions";
 
-export class SprikeyClient extends Client {
+export class SprikeyClient extends AkairoClient {
 
   readonly botIdentity = process.env.BOT_IDENTITY as "test" | "main";
 
@@ -20,44 +23,51 @@ export class SprikeyClient extends Client {
 
   readonly db: TableMetadata.Managers;
 
+  readonly handlers: HandlerInstances;
+
   readonly managers: ManagerInstances;
 
   readonly internalEvents = new EventEmitter();
 
-  constructor(readonly connection: InitiatedConnection) {
+  constructor(readonly connection: InitiatedConnection, readonly appState: AppState) {
 
-    super({
+    super({ ownerID: MASTER_ID }, {
       partials: [
-        `MESSAGE`,
-        `REACTION`,
-        `GUILD_MEMBER`,
-        `USER`
+        "MESSAGE",
+        "REACTION",
+        "GUILD_MEMBER",
+        "USER"
       ],
       presence: {
-        status: `idle`,
+        status: "idle",
         activity: {
-          name: `Initiating bot...`
+          name: "Initiating bot..."
         }
       },
-      fetchAllMembers: true
+      fetchAllMembers: true,
+      disableMentions: "everyone"
     });
 
     this.botOptions = connection.botOptions;
     this.db = connection.db;
 
     this.managers = {
-      command: new CommandManager(this),
-      listener: new ListenerManager(this),
       permission: new PermissionsManager(this)
     };
 
-    this.once(`ready`, () => this.generateInternalEvents());
+    this.handlers = {
+      command: new SprikeyCommandHandler(this),
+      inhibitor: new InhibitorHandler(this, { directory: path.join(__dirname, "../inhibitors/") }),
+      listener: new ListenerHandler(this, { directory: path.join(__dirname, "../listeners/") })
+    };
+
+    this.once("ready", () => this.generateInternalEvents());
 
   }
 
   async fetchGuilds(): Promise<{ MAIN_GUILD: Guild | null; TEST_GUILD: Guild }> {
     return {
-      MAIN_GUILD: this.botIdentity === `main` ? await this.guilds.fetch(GUILD.MAIN) : null,
+      MAIN_GUILD: this.botIdentity === "main" ? await this.guilds.fetch(GUILD.MAIN) : null,
       TEST_GUILD: await this.guilds.fetch(GUILD.TEST)
     };
   }
@@ -72,12 +82,16 @@ export class SprikeyClient extends Client {
 
   private generateInternalEvents(): void {
     void this.fetchGuilds()
-      .then(() => this.internalEvents.emit(`guildLoad`));
+      .then(() => this.internalEvents.emit("guildLoad"));
   }
 }
 
 interface ManagerInstances {
-  command: CommandManager;
-  listener: ListenerManager;
-  permission: PermissionsManager;
+  readonly permission: PermissionsManager;
+}
+
+interface HandlerInstances {
+  readonly command: CommandHandler;
+  readonly inhibitor: InhibitorHandler;
+  readonly listener: ListenerHandler;
 }
