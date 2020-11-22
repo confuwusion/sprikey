2bce50423bf5a5654f612d5d6c6807dc9c551a74
spuggle <immortalwarrior101@gmail.com>
2020-11-22 14:28:42 +0500

    [App] Fixes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import * as express from "express/index";
 
-export const app = express();
+const app = express();
 
-app.use(express.static(`public`));
-app.get(`/`, (_request, response) => {
+app.use(express.static("public"));
+app.get("/", (_request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
 });
 
-export const appListener: ReturnType<typeof app.listen> = app.listen(
-  process.env.PORT,
-  () => console.log(`Your app is listening on port ${process.env.PORT as string}`)
-);
+
+export function startApp(): AppState {
+  const appListener: AppState["appListener"] = app.listen(
+    process.env.PORT,
+    () => console.log(`Your app is listening on port ${process.env.PORT as string}`)
+  );
+
+  return { app, appListener };
+}
+
+export interface AppState {
+  readonly app: typeof app;
+  readonly appListener: ReturnType<typeof app.listen>;
+}
