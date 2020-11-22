20886003fdfbb734b6605d3e05cad5c72b1a8ca8
spuggle <immortalwarrior101@gmail.com>
2020-11-22 14:43:45 +0500

    [Listener, Inhibitor] Added client type override

---
+++ src/structs/SprikeyInhibitor.ts
@@ -0,0 +1,9 @@
+import { Inhibitor } from "discord-akairo";
+
+import { SprikeyClient } from "./SprikeyClient";
+
+export default class SprikeyInhibitor extends Inhibitor {
+
+  readonly client!: SprikeyClient;
+
+}
---
+++ src/structs/SprikeyListener.ts
@@ -0,0 +1,9 @@
+import { Listener } from "discord-akairo";
+
+import { SprikeyClient } from "./SprikeyClient";
+
+export default class SprikeyListener extends Listener {
+
+  readonly client!: SprikeyClient;
+
+}
